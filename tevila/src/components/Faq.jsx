df280c41919236ae3a979591a2958eab123a90ae
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-03-22 23:00:18 -0300

    Refactor Faq component for improved readability and performance

--- tevila/src/components/Faq.jsx
+++ tevila/src/components/Faq.jsx
@@ -11,27 +11,28 @@ function Faq() {
 
   return (
     <div className="faq-container">
-      {data.map((category, index) => (
-        <div key={index} className="faq-category">
-          <div className="faq-header" onClick={() => toggleCategory(index)}>
+      {data.map((category, categoryIndex) => (
+        <div key={categoryIndex} className="faq-category">
+          <div className="faq-header" onClick={() => toggleCategory(categoryIndex)}>
             <div className="faq-image-container">
               <img src={category.imagen} alt={category.categoria} className="faq-image" />
               <div className="faq-overlay">
                 <h2 className="faq-category-title">{category.categoria}</h2>
               </div>
               <div className="arrow-over">
-              <img
+                <img
                   src="/img/down.svg"
                   alt="arrow"
-                  className={`faq-arrow ${openCategory === index ? "open" : ""}`}
+                  className={`faq-arrow ${openCategory === categoryIndex ? "open" : ""}`}
                 />
               </div>
             </div>
           </div>
-          {openCategory === index && (
+          
+          {openCategory === categoryIndex && (
             <div className="faq-list">
-              {category.preguntas.map((faq, idx) => (
-                <div key={idx} className="faq-item">
+              {category.preguntas.map((faq, questionIndex) => (
+                <div key={questionIndex} className="faq-item">
                   <p className="faq-question">{faq.pregunta}</p>
                   <p className="faq-answer">{faq.respuesta}</p>
                 </div>
