655b6a7e701ebf3bacdcf2fe077523cafdd3849a
Brigitte Blau <brigitteyaelblau@gmail.com>
2025-04-01 10:20:45 -0300

    123

--- tevila/src/routes.jsx
+++ tevila/src/routes.jsx
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import ScrollToTop from "./Scroll";
 export default function AppRoutes() {
   return (
+    <>
+    <ScrollToTop />
       <Routes>
+        
         <Route path="/" element={<Home/>} />
   
       </Routes>
-
+</>
   );
 }
